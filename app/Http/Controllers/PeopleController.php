acf785b79692c3179830c7a3c839f91793667026
Pedro de Robles <piterddr@gmail.com>
2022-02-04 07:00:05 -0300

    Creacion del modelo People con -mfc configurado y vista index de people con formulario para agregar personas

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\People;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    public function index()
+    {
+        return view('people.index');
+    }
+
+    public function create()
+    {
+        return view('people.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'  => 'required',
+            'place' => 'required',
+        ]);
+
+        $person = People::create([
+            'name'  => $request->name,
+            'place' => $request->place,
+        ]);
+
+        return back();
+    }
+
+    public function show(People $people)
+    {
+        //
+    }
+
+    public function edit(People $people)
+    {
+        return view('people.edit', compact('people'));
+    }
+
+    public function update(Request $request, People $people)
+    {
+        $request->validate([
+            'name'  => 'required',
+            'place' => 'required',
+        ]);
+
+        $people->update($request->all());
+
+        return redirect()->route('people.index');
+    }
+
+    public function destroy(People $people)
+    {
+        $people->delete();
+
+        return redirect()->route('people.index');
+    }
+}

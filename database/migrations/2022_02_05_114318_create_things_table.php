74d2a80352d090eb334ba136189d1b8b12a3ea01
Pedro de Robles <piterddr@gmail.com>
2022-02-05 08:47:28 -0300

    Correccion en las migraciones de la tabla 'states' y 'thing' 2

---
+++ database/migrations/2022_02_05_114318_create_things_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('things', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('type_id')->unsigned();
+            $table->bigInteger('state_id')->unsigned();
+
+            $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('things');
+    }
+}

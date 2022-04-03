72bd5633dc03575cebc6becab27bd602b340737f
Pedro de Robles <piterddr@gmail.com>
2022-04-03 01:34:06 -0300

    livewire

---
+++ app/Http/Livewire/PersonComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PersonComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.person-component');
+    }
+}
